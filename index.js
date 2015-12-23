4969fecffa3aae8afa0515d29e0977455ef5dcd9
Alex Grant <alex@localnerve.com>
2015-12-23 14:13:46 -0500

    remove string proto, clean explicit encoding

--- index.js
+++ index.js
@@ -3,67 +3,71 @@
 var stream = require('stream');
 var util = require('util');
 
-function SitemapStream() {
-	stream.Transform.call(this, { objectMode: true });
-	this._headOutputted = false;
+var streamEnc = 'utf8';
+
+function SitemapStream () {
+  stream.Transform.call(this, {
+    objectMode: true,
+    encoding: streamEnc
+  });
+  this._headOutputted = false;
 }
 
 util.inherits(SitemapStream, stream.Transform);
 
-if (!String.prototype.encodeHTML) {
-	String.prototype.encodeHTML = function () {
-		return this.replace(/&/g, '&amp;')
-			.replace(/</g, '&lt;')
-			.replace(/>/g, '&gt;')
-			.replace(/"/g, '&quot;');
-	};
+function encodeHTML (str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
 }
 
-SitemapStream.prototype._transform = function(chunk, encoding, callback) {
-	if (!this._headOutputted) {
-		this.push('<?xml version="1.0" encoding="UTF-8"?>\r\n', 'utf-8');
-		this.push('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', 'utf-8');
-		this._headOutputted = true;
-	}
+SitemapStream.prototype._transform = function (chunk, encoding, callback) {
+  if (!this._headOutputted) {
+    this.push('<?xml version="1.0" encoding="UTF-8"?>\r\n', streamEnc);
+    this.push('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', streamEnc);
+    this._headOutputted = true;
+  }
 
-	if (typeof(chunk) === 'string') {
-		chunk = {
-			loc: chunk
-		};
-	}
+  if (typeof(chunk) === 'string') {
+    chunk = {
+      loc: chunk
+    };
+  }
 
-	this.push('<url>', 'utf-8');
-	this.push('<loc>', 'utf-8');
-	this.push(chunk.loc.encodeHTML(), 'utf-8');
-	this.push('</loc>', 'utf-8');
+  this.push('<url>', streamEnc);
+  this.push('<loc>', streamEnc);
+  this.push(encodeHTML(chunk.loc), streamEnc);
+  this.push('</loc>', streamEnc);
 
-	if (chunk.lastmod) {
-		this.push('<lastmod>', 'utf-8');
-		this.push(chunk.lastmod.encodeHTML(), 'utf-8');
-		this.push('</lastmod>', 'utf-8');
-	}
+  if (chunk.lastmod) {
+    this.push('<lastmod>', streamEnc);
+    this.push(encodeHTML(chunk.lastmod), streamEnc);
+    this.push('</lastmod>', streamEnc);
+  }
 
-	if (chunk.changefreq) {
-		this.push('<changefreq>', 'utf-8');
-		this.push(chunk.changefreq.encodeHTML(), 'utf-8');
-		this.push('</changefreq>', 'utf-8');
-	}
+  if (chunk.changefreq) {
+    this.push('<changefreq>', streamEnc);
+    this.push(encodeHTML(chunk.changefreq), streamEnc);
+    this.push('</changefreq>', streamEnc);
+  }
 
-	if (chunk.priority) {
-		this.push('<priority>', 'utf-8');
-		this.push(chunk.priority.encodeHTML(), 'utf-8');
-		this.push('</priority>', 'utf-8');
-	}
+  if (chunk.priority) {
+    this.push('<priority>', streamEnc);
+    this.push(encodeHTML(chunk.priority), streamEnc);
+    this.push('</priority>', streamEnc);
+  }
 
-	this.push('</url>', 'utf-8');
-	callback();
+  this.push('</url>', streamEnc);
+  callback();
 };
 
-SitemapStream.prototype._flush = function(callback) {
-	this.push('</urlset>');
-	callback();
+SitemapStream.prototype._flush = function (callback) {
+  this.push('</urlset>', streamEnc);
+  callback();
 };
 
-module.exports = function() {
-	return new SitemapStream();
+module.exports = function () {
+  return new SitemapStream();
 };
